58d33a1e7fa57b774e266f7a19fbfef3f2756024
lfiorenzo19 <leofiorenzo9@gmail.com>
2024-09-14 12:43:26 -0300

    arreglando errores

--- trabajoIntegrador/online-store/src/components/Map.jsx
+++ trabajoIntegrador/online-store/src/components/Map.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
 
 function Map() {
