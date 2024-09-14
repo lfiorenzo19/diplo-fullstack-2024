7809d7d29f57ee408b058e424acd3ffebcf1197e
lfiorenzo19 <leofiorenzo9@gmail.com>
2024-09-14 13:32:30 -0300

    correcciones

--- trabajoIntegrador/online-store/src/components/Map.jsx
+++ trabajoIntegrador/online-store/src/components/Map.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility';
 
@@ -11,12 +10,12 @@ function Map() {
     };
 
 return (
-    <MapContainer center={[position.lat,position.lng]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[position.lat,position.lng]} zoom={16} scrollWheelZoom={false}>
     <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
     <Marker position={position}>
-        <Popup>Papelera Cesar <br /> Distribuidor de Bolasas</Popup>
+        <Popup>Papelera Cesar - Distribuidor</Popup>
     </Marker>
     </MapContainer>
 );
